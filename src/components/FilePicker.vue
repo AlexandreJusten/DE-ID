3225cb8fefa57f86c45a484129b9c1bcf2abaf6b
losthunter52 <maherdaoud52@gmail.com>
2023-09-10 22:23:54 -0300

    file picker v0.1

--- src/components/FilePicker.vue
+++ src/components/FilePicker.vue
@@ -1,78 +1,205 @@
 <template>
-  <div class="column" style="height: 150px">
-    <div class="col">
-      <q-file
-        style="
-          width: 400px;
-          height: 100px;
-          background-color: rgba(195, 195, 195, 0.682);
-        "
-        v-model="files"
-        label="Drop Them Here"
-        filled
-        counter
-        :counter-label="counterLabelFn"
-        max-files="1"
-        multiple
-        class="custom-file-input"
-        :disable="!userLoggedIn"
-        accept=".json, .csv"
-        @rejected="onRejected"
-      >
-        <template v-slot:prepend>
-          <q-icon name="attach_file" class="file-icon" />
-        </template>
-      </q-file>
-      <q-tooltip
-        v-if="!userLoggedIn"
-        anchor="top middle"
-        style="font-size: 15px"
-      >
-        You need to login to enable uploading
-      </q-tooltip>
+  <div class="q-pa-md q-gutter-sm">
+    <q-file
+      style="width: 80%; min-width: 800px; background-color: rgba(195, 195, 195, 0.682)"
+      v-model="files"
+      label="Drop a File Here to Start"
+      filled
+      max-files="1"
+      multiple
+      class="custom-file-input"
+      :disable="!userLoggedIn"
+      accept=".csv"
+      @rejected="onRejected"
+      v-on:update:model-value="uploadFile"
+    >
+      <template v-slot:prepend>
+        <q-icon name="attach_file" class="file-icon" />
+      </template>
+    </q-file>
+    <q-tooltip v-if="!userLoggedIn" anchor="top middle" style="font-size: 15px">
+      You need to login to enable uploading
+    </q-tooltip>
+    <div v-if="columns && columns.length > 0">
+      <q-card>
+      <q-card-section>
+      <h4>Parameterization:</h4>
+      <ul class="column-list">
+        <li v-for="(column, index) in columns" :key="index" class="column-item">
+          {{ column }}
+          <q-checkbox v-model="sensitiveColumns[column]" label="Sensitive" />
+          <q-checkbox v-model="diversityColumns[column]" label="Diversity" />
+          <q-checkbox v-model="closenessColumns[column]" label="Closeness" />
+          <q-select
+            v-model="selectedColumns[column]"
+            :options="firstSelectorOptions"
+            label="Select Technique"
+            @update:model-value="clearSecondSelector(column)"
+          />
+          <q-select
+            v-if="selectedColumns[column] !== null"
+            v-model="selectedSecondSelectors[column]"
+            :options="secondSelectorOptions[selectedColumns[column]]"
+            label="Select Method"
+          />
+        </li>
+      </ul>
+      </q-card-section>
+      </q-card>
     </div>
-    <div class="col" style="margin-top: 15vh; text-align: center">
-      <q-btn push color="primary" label="Upload" />
+    <div>
+      <q-btn
+        label="Show Payload"
+        color="primary"
+        @click="showPayload"
+        style="margin-top: 20px"
+      />
     </div>
+    <q-dialog v-model="payloadDialog">
+      <q-card>
+        <q-card-section>
+          <div class="text-h6">Payload</div>
+        </q-card-section>
+        <q-card-section>
+          <pre>{{ payload }}</pre>
+        </q-card-section>
+        <q-card-actions align="right">
+          <q-btn flat label="OK" color="primary" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
   </div>
 </template>
-<style scoped>
-.custom-file-input {
-  background-color: rgba(192, 192, 192, 0.3); /* Cor cinza transparente */
-  border: none;
-  padding: 20px;
-  border-radius: 10px;
-  box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.4); /* Sombra pequena */
-  transition: background-color 0.3s, box-shadow 0.3s;
-}
-
-.custom-file-input:focus {
-  outline: none;
-  background-color: rgba(
-    192,
-    192,
-    192,
-    0.5
-  ); /* Cor cinza transparente ao focar */
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2); /* Sombra um pouco maior ao focar */
-}
-
-.file-icon {
-  font-size: 36px;
-  color: #007bff;
-}
-</style>
 
 <script>
-import { ref, watch } from "vue";
+import { ref, watch, computed } from "vue";
 import { Dialog } from "quasar";
 import { useAuthStore } from "stores/user";
+import Papa from "papaparse";
 
 export default {
   setup() {
     const files = ref(null);
     const authStore = useAuthStore();
     const userLoggedIn = ref(authStore.isAuthenticated);
+    const fileContent = ref(null);
+    const columns = ref([]);
+    const selectedColumns = ref({});
+    const firstSelectorOptions = ref([
+      "encrypt",
+      "generalize",
+      "hash",
+      "mask",
+      "null_out",
+      "perturb",
+      "pseudonymize",
+      "swap",
+    ]);
+    const selectedSecondSelectors = ref({});
+
+    const secondSelectorOptions = {
+      encrypt: ["chacha20", "aes", "salsa20"],
+      generalize: ["percent", "age"],
+      hash: ["md5", "sha1", "sha256"],
+      mask: [
+        "full",
+        "range",
+        "first_n_characters",
+        "last_n_characters",
+        "email",
+        "cpf",
+      ],
+      null_out: ["columns"],
+      perturb: [
+        "date",
+        "numeric_range",
+        "numeric_gaussian",
+        "numeric_laplacian",
+      ],
+      pseudonymize: ["columns", "rows"],
+      swap: ["columns", "rows"],
+    };
+
+    const sensitiveColumns = ref({});
+    const diversityColumns = ref({});
+    const closenessColumns = ref({});
+
+    const payloadDialog = ref(false);
+
+    const payload = computed(() => {
+      const executionParameters = {};
+      const sensitiveColumnsArray = [];
+      const diversityColumnsArray = [];
+      const closenessColumnsArray = [];
+
+      for (const column of columns.value) {
+        const technique = selectedColumns.value[column];
+        const method = selectedSecondSelectors.value[column];
+
+        if (technique && method) {
+          const key = `${technique}.${method}`;
+          if (!executionParameters[key]) {
+            executionParameters[key] = {
+              algorithm: key,
+              columns: [],
+            };
+          }
+          executionParameters[key].columns.push(column);
+        }
+
+        if (sensitiveColumns.value[column]) {
+          sensitiveColumnsArray.push(column);
+        }
+
+        if (diversityColumns.value[column]) {
+          diversityColumnsArray.push(column);
+        }
+
+        if (closenessColumns.value[column]) {
+          closenessColumnsArray.push(column);
+        }
+      }
+
+      const payload = {
+        execution_parameters: Object.values(executionParameters),
+        sensitive_columns: sensitiveColumnsArray,
+        diversity_columns: diversityColumnsArray,
+        closeness_columns: closenessColumnsArray,
+        data: fileContent.value,
+      };
+
+      return JSON.stringify(payload, null, 2);
+    });
+
+    function uploadFile() {
+      try {
+        if (files.value) {
+          const file = files.value[0];
+          const fileReader = new FileReader();
+          fileReader.onload = (event) => {
+            Papa.parse(event.target.result, {
+              header: true,
+              complete: (results) => {
+                columns.value = Object.keys(results.data[0]);
+                fileContent.value = results.data;
+                console.log("Parsed CSV data:", fileContent.value);
+                if (fileContent.value.length > 0) {
+                  console.log("Columns:", columns.value);
+                  selectedColumns.value = {};
+                  selectedSecondSelectors.value = {};
+                }
+              },
+              error: (error) => {
+                console.error("Error parsing CSV:", error.message);
+              },
+            });
+          };
+          fileReader.readAsText(file);
+        }
+      } catch (error) {
+        console.error("Error reading file:", error);
+      }
+    }
 
     function counterLabelFn({ totalSize, filesNumber, maxFiles }) {
       return `${filesNumber} files of ${maxFiles} | ${totalSize}`;
@@ -86,9 +213,14 @@ export default {
       });
     }
 
+    function clearSecondSelector(column) {
+      selectedSecondSelectors.value[column] = null;
+    }
+
     watch(authStore.isAuthenticated, (newValue) => {
       userLoggedIn.value = newValue;
     });
+
     watch(userLoggedIn, (newValue) => {
       if (!newValue && files.value) {
         files.value = null;
@@ -96,11 +228,55 @@ export default {
       }
     });
 
+    function showPayload() {
+      payloadDialog.value = true;
+    }
+
     return {
       files,
       counterLabelFn,
       userLoggedIn,
+      fileContent,
+      uploadFile,
+      selectedColumns,
+      firstSelectorOptions,
+      columns,
+      selectedSecondSelectors,
+      secondSelectorOptions,
+      clearSecondSelector,
+      sensitiveColumns,
+      diversityColumns,
+      closenessColumns,
+      payload,
+      showPayload,
+      payloadDialog,
     };
   },
 };
 </script>
+
+<style>
+.column-list {
+  list-style: none;
+  padding: 0;
+}
+
+.column-item {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 10px;
+  padding: 10px;
+  background-color: #f0f0f0;
+  border-radius: 5px;
+}
+
+.column-item q-select {
+  flex: 1;
+  margin-left: 10px;
+}
+
+.column-item q-checkbox {
+  margin-left: 10px;
+}
+</style>
